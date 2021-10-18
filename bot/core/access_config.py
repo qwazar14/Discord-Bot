d29791f4383007a7606f4603fb82decb47dfaa35
qwazar14 <zenez89@gmail.com>
2021-10-18 23:22:13 +0300

    Added Role IDs to roles_config.py

--- bot/core/access_config.py
+++ bot/core/access_config.py
@@ -1,6 +1,7 @@
 settings = {
-    'token': 'ODk5Njg3MjY1NjU0MTQ5MTUx.YW2ZDw.Qykl9kv-tLyk0OVBhtk5fiBtJJ8',
-    'bot': 'testingwtbot1',
-    'id': 899687265654149151,
-    'prefix': '!'
+    'botToken': 'ODk5Njg3MjY1NjU0MTQ5MTUx.YW2ZDw.Qykl9kv-tLyk0OVBhtk5fiBtJJ8',
+    'botName': 'Warthunder Bot',
+    'botId': 899687265654149151,
+    'botPrefix': '!',
+    'guildId': 821674992370581554
 }
