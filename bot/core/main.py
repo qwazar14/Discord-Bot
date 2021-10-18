16ccae399b6ba54f7453011ce9da6166826441e3
qwazar14 <zenez89@gmail.com>
2021-10-18 23:22:55 +0300

    Added roles tank, plane, ship

--- bot/core/main.py
+++ bot/core/main.py
@@ -1,23 +1,57 @@
 import discord
 from discord.ext import commands
-from discord.utils import get
+
+import roles_config
 from access_config import settings as settings
 
-client = commands.Bot(command_prefix=settings['prefix'])
+client = commands.Bot(command_prefix=settings['botPrefix'], help_command=None)
 
 
 @client.event
 async def on_ready():
-
     print('[LOG] Bot is ready!')
 
 
 @client.command()
-async def hello(ctx):  # Создаём функцию и передаём аргумент ctx.
-    # Объявляем переменную author и записываем туда информацию об авторе.
-    author = ctx.message.author
+async def help(ctx):
+    print('[LOG] called command "help"')
+    await ctx.send("Custom help command")
+    print('[LOG] Help command done!')
 
-    await ctx.send(f'Hello, {author.mention}!')
+
+@client.command()
+async def tank(ctx, user: discord.Member):
+    print(f'[LOG] {user} called command "tank"')
+    guild = client.get_guild(settings['guildId'])
+    await user.add_roles(guild.get_role(roles_config.unit_roles['tanks']))
+    await ctx.message.add_reaction('✅')
+    await ctx.message.delete(delay=5)
+    print('[LOG] Set role "Tank" command done!')
 
 
-client.run(settings['token'])
+@client.command()
+async def plane(ctx, user: discord.Member):
+    print(f'[LOG] {user} called command "plane"')
+    guild = client.get_guild(settings['guildId'])
+    await user.add_roles(guild.get_role(roles_config.unit_roles['planes']))
+    await ctx.message.add_reaction('✅')
+    await ctx.message.delete(delay=5)
+    print('[LOG] Set role "Plane" command done!')
+
+
+@client.command()
+async def ship(ctx, user: discord.Member):
+    print(f'[LOG] {user} called command "ship"')
+    guild = client.get_guild(settings['guildId'])
+    await user.add_roles(guild.get_role(roles_config.unit_roles['ship']))
+    await ctx.message.add_reaction('✅')
+    await ctx.message.delete(delay=5)
+    print('[LOG] Set role "Ship" command done!')
+
+@client.command()
+async def rules():
+    pass
+
+
+
+client.run(settings['botToken'])
