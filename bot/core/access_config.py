341c7a3c5299af80d7c0db6b13686120502747e4
Maksym Grebeniuk <zenez89@gmail.com>
2021-10-20 01:23:07 +0300

    Delete access_config.py

--- bot/core/access_config.py
+++
@@ -1,7 +0,0 @@
-settings = {
-    'botToken': 'OTAwMDQ5OTIzMzg5NjYxMzA1.YW7qzw.i7Te8w1Ugup4aZni7wrf2XAi6Oc',
-    'botName': 'Romank99 Test bot',
-    'botId': 900049923389661305,
-    'botPrefix': '+',
-    'guildId': 821674992370581554
-}
