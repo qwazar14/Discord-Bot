9be87b3e266e0b44ed52715b0b875adfed15c948
GreedMou <38079451+GreedMou@users.noreply.github.com>
2021-10-20 22:17:41 +0300

    Update parse_stats.py
    Ну заработай(

--- bot/core/modules/user/parse_stats.py
+++ bot/core/modules/user/parse_stats.py
@@ -6,6 +6,7 @@ async def get_statistics(ctx, nickname, mode):
     await clear_user_message.clear_last_user_message(ctx)
     if nickname is None:
         nickname = ctx.author.nick
+    # Git позязя заработай
     nickname = re.search("(?<=\])(.*?)(?=\()", nickname).group(0).strip()
     link = f"https://thunderskill.com/userbars/z/e/{nickname}/ru-1-combined-{mode}.png"
     print(link)
