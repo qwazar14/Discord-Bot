4156511f090d49f0d99f224c55dea03098cf8715
GreedMou <38079451+GreedMou@users.noreply.github.com>
2021-10-20 21:54:04 +0300

    Change nickname parser to Regex
    Change nickname parser to Regex
    Add .pyc files to .gitignore

--- bot/core/modules/user/parse_stats.py
+++ bot/core/modules/user/parse_stats.py
@@ -5,10 +5,7 @@ async def get_statistics(ctx, nickname, mode):
     await clear_user_message.clear_last_user_message(ctx)
     if nickname is None:
         nickname = ctx.author
-    nickname = f'{nickname.nick}'
-    nickname = nickname.split('] ')
-    nickname = nickname[1].split(' (')
-    nickname = nickname[0]
+    nickname = re.search(r"(?<=\])(.*?)(?=\()", nickname).strip()
 
     link = f"https://thunderskill.com/userbars/z/e/{nickname}/ru-1-combined-{mode}.png"
     print(link)
